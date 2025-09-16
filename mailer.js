4141849eea9508cb37c00bfdf8ff1036c50947c0
Venkatesh200510 <venkateshsgounskar@gmail.com>
2025-09-16 23:39:09 +0530

    resend api

--- mailer.js
+++ mailer.js
@@ -1,11 +1,24 @@
-const nodemailer = require("nodemailer");
+const { Resend } = require("resend");
 
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: "dams.project25@gmail.com", // your email
-    pass: "totl oecx oktp kqtv",      // your app password
+const resend = new Resend(process.env.re_BNTnJcBj_Fxj6hzpwS2tvDL4ECY1iHRiK);
+
+const transporter = {
+  async sendMail({ to, subject, text, html }) {
+    try {
+      const response = await resend.emails.send({
+        from: "dams.project25@gmail.com", // must be verified in Resend
+        to,
+        subject,
+        text,
+        html,
+      });
+      console.log("Email sent:", response);
+      return response;
+    } catch (error) {
+      console.error("Error sending email:", error);
+      throw error;
+    }
   },
-});
+};
 
 module.exports = { transporter };
